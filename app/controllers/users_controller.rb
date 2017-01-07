0f70ba3747491d9dc27a0b49d30756525ffaede4
renan-garcia <email@renangarcia.me>
2017-01-07 02:05:37 -0200

    Feito ate o capitulo 9 do livro

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+before_action :set_user , only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: 'Cadastro criado com sucesso!'
+    else
+      render action: :new
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'Cadastro atualizado com sucesso.'
+    else
+      render action: :edit
+    end    
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :full_name, :location, :password,:password_confirmation, :bio)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170106004520) do
+ActiveRecord::Schema.define(version: 20170107034844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,11 +26,11 @@ ActiveRecord::Schema.define(version: 20170106004520) do
   create_table "users", force: :cascade do |t|
     t.string   "full_name"
     t.string   "email"
-    t.string   "password"
     t.string   "location"
     t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
